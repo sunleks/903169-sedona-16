f3eb135cf8b438f6f97e4fbacb11848bd397144e
sunleks@users <67sasha1996@mail.ru>
2019-05-06 23:43:07 +0300

    form.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require('gulp-csso');
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -35,3 +37,15 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+var imagemin = require("gulp-imagemin");
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+      ]))
+
+    .pipe(gulp.dest("source/img"));
+})
